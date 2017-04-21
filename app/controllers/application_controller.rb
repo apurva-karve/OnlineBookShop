e7dc2eb9576cadcc11a50c0908918964e8bd8c4d
apurva <akarve@thoughtworks.com>
2017-04-21 11:07:07 +0530

    Functionality for handling the exceptions is added at application level

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_filter :authenticate_user!
   protect_from_forgery
+
+  rescue_from ActiveRecord::RecordNotFound, :with=> :error_render_method
+
+  def error_render_method
+    respond_to do |format|
+            format.html { render file: "public/404.html"}
+            format.json { render json: @book.errors, status: :not_found }
+    end
+    true
+  end
+
 end
